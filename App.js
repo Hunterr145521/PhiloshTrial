acbb1efee62f9d2a90c10d910989f3b01e1972c4
Hunterr145521 <s.satyamsingh17@gmail.com>
2019-01-25 10:35:48 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Philosh is Under Devlopment till last week of april</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
